06a4645a9280893dd57e896566475a1ec023b19b
Cameron Weston <cameronbweston@gmail.com>
2021-08-14 10:17:43 -0500

    Begin stubbing up methods for anime controller and animeAPI services model

---
+++ controllers/anime.js
@@ -0,0 +1,19 @@
+import axios from 'axios'
+
+export {
+  search,
+  getRandomShowsForGameStart,
+  addToUserCollection
+}
+
+function search(req, res) {
+
+}
+
+function getRandomShowsForGameStart(req, res) {
+
+}
+
+function addToUserCollection(req, res) {
+  //Pass in variable to decide if it will be saved to user collection 'want to watch', 'watching', or 'will watch'
+}
---
+++ src/services/animeService.js
@@ -0,0 +1,35 @@
+//All requests to AniList GraphQL API must be made as a POST request to https://graphql.anilist.co
+
+/*
+var url = 'https://graphql.anilist.co',
+    options = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json',
+        },
+        body: JSON.stringify({
+            query: query,
+            variables: variables
+        })
+    };
+Make the HTTP Api request
+fetch(url, options).then(handleResponse)
+                   .then(handleData)
+                   .catch(handleError);
+
+function handleResponse(response) {
+    return response.json().then(function (json) {
+        return response.ok ? json : Promise.reject(json);
+    });
+}
+
+function handleData(data) {
+    console.log(data);
+}
+
+function handleError(error) {
+    alert('Error, check console');
+    console.error(error);
+}
+*/
