f8d6059c81f10e7cb7f6c2049f9d14649ba15066
Cameron Weston <cameronbweston@gmail.com>
2021-08-15 13:38:24 -0500

    Copy all profile code from binge. Leave methods and function calls the same so we can use them for our purposes

--- src/services/profileService.js
+++ src/services/profileService.js
@@ -14,3 +14,25 @@ export function getAllProfiles() {
   {mode: 'cors'})
   .then(res => res.json())
 }
+
+export function friend(id) {
+  return fetch(
+    `${BASE_URL}/friend/${id}`,
+    {
+      method: 'PATCH',
+      headers: { Authorization: "Bearer " + tokenService.getToken() }
+    },
+    { mode: "cors" }
+    ).then((res) => res.json())
+}
+
+export function unfriend(id) {
+  return fetch(
+    `${BASE_URL}/unfriend/${id}`,
+    {
+      method: 'PATCH',
+      headers: { Authorization: "Bearer " + tokenService.getToken() }
+    },
+    { mode: "cors" }
+    ).then((res) => res.json())
+}
