3e04db6aa67a36b80ead353819ffabfbf88cb243
Cameron Weston <cameronbweston@gmail.com>
2021-08-15 17:12:04 -0500

    Import ProfileDetails and ProfileList from binge so we can use them for our friends and user profiles pages

---
+++ src/components/ProfileCard/ProfileCard.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const ProfileCard = () => {
+  return (
+    <>
+    </>
+  );
+}
+ 
+export default ProfileCard;
---
+++ src/pages/ProfileDetails/ProfileDetails.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+
+const ProfileDetails = ({ location, userProfile }) => {
+  const { profile } = location.state
+  const movies = location.state.profile.media.filter(media => media.type === 'movie')
+  const tvs = profile.media.filter(media => media.type === 'tv')
+  return (
+    <>
+      <h1>{profile.name}'s Deets</h1>
+
+      <h2>Friends</h2>
+      {profile.friends.map(profile => 
+        <>
+          <h3 key={profile._id}>
+            {profile.name}
+          </h3>
+        </>
+      )}
+      <h2>TV Shows</h2>
+
+      <h2>Movies</h2>
+      {movies.map(movie =>
+        <a href={`/movies/${movie.api_id}`}>
+          <img src={`https://image.tmdb.org/t/p/w200/${movie.poster_path}`} alt="poster" />
+        </a>
+      )}
+    </>
+  );
+}
+ 
+export default ProfileDetails;
