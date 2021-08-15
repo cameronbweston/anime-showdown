cba1a20f798d79347bc969fc84cf882b3cab45a7
Cameron Weston <cameronbweston@gmail.com>
2021-08-15 17:15:15 -0500

    Remove some ProfileDetails so that we can make it our own

--- src/pages/ProfileDetails/ProfileDetails.jsx
+++ src/pages/ProfileDetails/ProfileDetails.jsx
@@ -2,11 +2,11 @@ import React from 'react'
 
 const ProfileDetails = ({ location, userProfile }) => {
   const { profile } = location.state
-  const movies = location.state.profile.media.filter(media => media.type === 'movie')
-  const tvs = profile.media.filter(media => media.type === 'tv')
+  // const movies = location.state.profile.media.filter(media => media.type === 'movie')
+  // const tvs = profile.media.filter(media => media.type === 'tv')
   return (
     <>
-      <h1>{profile.name}'s Deets</h1>
+      <h1>{profile.name}'s Profile</h1>
 
       <h2>Friends</h2>
       {profile.friends.map(profile => 
@@ -19,11 +19,6 @@ const ProfileDetails = ({ location, userProfile }) => {
       <h2>TV Shows</h2>
 
       <h2>Movies</h2>
-      {movies.map(movie =>
-        <a href={`/movies/${movie.api_id}`}>
-          <img src={`https://image.tmdb.org/t/p/w200/${movie.poster_path}`} alt="poster" />
-        </a>
-      )}
     </>
   );
 }
