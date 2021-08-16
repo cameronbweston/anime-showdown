d3e85240e9109248bfa869bf807bbfe4d74b3315
Cameron Weston <cameronbweston@gmail.com>
2021-08-16 09:50:41 -0500

    Stub up and create  GamePage component

---
+++ src/pages/GamePage/GamePage.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class GamePage extends Component {
+  state = { 
+
+   }
+
+  render() { 
+    return ( 
+      <>
+      </>
+     );
+  }
+}
+ 
+export default GamePage;
